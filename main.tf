40fd319fc67362328063f1fb11cbac1c670473c2
Tutuianu <Danut.Tutuianu@uscellular.com>
2025-03-03 20:52:33 -0600

    reformat with terraform fmt command

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "3.26.0"
+      source     = "hashicorp/aws"
+      version    = "3.26.0"
       access_key = var.AWS_ACCESS_KEY_ID
       secret_key = var.AWS_SECRET_ACCESS_KEY
     }
